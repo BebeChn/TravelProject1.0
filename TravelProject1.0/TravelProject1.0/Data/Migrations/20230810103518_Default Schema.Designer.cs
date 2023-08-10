58fa210892273ee80c766e74582ed1c932d8dc27
Bebe <fastic55123@gmail.com>
2023-08-10 23:30:49 +0800

    login

--- TravelProject1.0/TravelProject1.0/Data/Migrations/20230810103518_Default Schema.Designer.cs
+++ TravelProject1.0/TravelProject1.0/Data/Migrations/20230810103518_Default Schema.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TravelProject1._0.Data;
 
@@ -11,9 +12,11 @@ using TravelProject1._0.Data;
 namespace TravelProject1._0.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230810103518_Default Schema")]
+    partial class DefaultSchema
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
