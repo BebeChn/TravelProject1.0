b8b1cedd921c5af36dbfb048df5e275991025c47
VMWARE\yu <yu20334.sam@gmail.com>
2023-08-19 09:10:16 +0800

    no message

---
+++ TravelProject1.0/TravelProject1.0/Areas/Admin/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TravelProject1._0.Areas.Admin.Models;
+
+public partial class Product
+{
+    public int ProductId { get; set; }
+
+    public int CategoryId { get; set; }
+
+    public string ProductName { get; set; } = null!;
+
+    public decimal Price { get; set; }
+
+    public string? Describe { get; set; }
+
+    public string? Describe1 { get; set; }
+
+    public string? Describe2 { get; set; }
+}
