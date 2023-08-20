dcd4ab3ee4ecc5b6dea42104a4c95ea8f6ecc2c4
VMWARE\yu <yu20334.sam@gmail.com>
2023-08-20 11:25:43 +0800

    3

--- TravelProject1.0/TravelProject1.0/Areas/Admin/Controllers/AdminController.cs
+++ TravelProject1.0/TravelProject1.0/Areas/Admin/Controllers/AdminController.cs
@@ -1,13 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TravelProject1._0.Models;
 
 namespace TravelProject1._0.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class AdminController : Controller
     {
+        private readonly TravelUserContext _db;
+
+        public AdminController(TravelUserContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Admin()
         {
-            return View();
+            return _db.User !-=null?
+                View(_db.User);
         }
 
         public IActionResult Merchandise()
