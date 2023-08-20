e198f2ca4265aebd3d85676d47bc887484b15070
VMWARE\yu <yu20334.sam@gmail.com>
2023-08-20 18:45:58 +0800

    4

--- TravelProject1.0/TravelProject1.0/Areas/Admin/Controllers/AdminController.cs
+++ TravelProject1.0/TravelProject1.0/Areas/Admin/Controllers/AdminController.cs
@@ -1,22 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TravelProject1._0.Models;
+using TravelProject1._0.ViewModel;
 
 namespace TravelProject1._0.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class AdminController : Controller
     {
-        private readonly TravelUserContext _db;
+        private readonly TravelProjectContext _db;
 
-        public AdminController(TravelUserContext db)
+        public AdminController(TravelProjectContext db)
         {
             _db = db;
         }
 
-        public IActionResult Admin()
+        public async  Task<IActionResult> Admin()
         {
-            return _db.User !-=null?
-                View(_db.User);
+            var result = _db.Users.Select(x => new AdminViewModel
+            {
+                Address = x.Address,
+                Gender = x.Gender,
+                Name = x.Name,
+                UserId = x.UserId
+            }).ToList();
+            return View(result);
         }
 
         public IActionResult Merchandise()
---
+++ TravelProject1.0/TravelProject1.0/ViewModel/AdminViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace TravelProject1._0.ViewModel
+{
+    public class AdminViewModel
+    {
+        [DisplayName("xxxxx")]
+        public int UserId { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string? Gender { get; set; }
+
+        public string? Address { get; set; }
+    }
+}
