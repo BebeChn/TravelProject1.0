1bad064ceee135759054f288f3ddf06202469389
Allen <allen955103@gmail.com>
2023-08-21 16:20:58 +0800

    新增 景點跟交通Api，前端取得Api渲染畫面，交通金額排序

---
+++ TravelProject1.0/TravelProject1.0/Controllers/Api/TransportApiController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TravelProject1._0.Models;
+using TravelProject1._0.Models.DTO;
+
+namespace TravelProject1._0.Controllers.Api
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class TransportApiController : ControllerBase
+    {
+        private readonly TravelProjectContext _dbContext;
+
+        public TransportApiController(TravelProjectContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        //取得交通種類的商品
+        [HttpGet]
+        public async Task<IEnumerable<TransportDTO>> GetCategoryByTransport()
+        {
+            return _dbContext.Products.Where(c => c.Id == 3).Select(p => new TransportDTO
+            {
+                Name = p.Name,
+                Price = p.Price,
+                MainDescribe = p.MainDescribe,
+            });
+        }
+
+        //排序商品的價格
+        //低到高
+        public async Task<IEnumerable<TransportDTO>> TransportOrderbyPrice()
+        {
+            return _dbContext.Products.Where(w => w.Id == 3).OrderBy(o => o.Price).Select(p => new TransportDTO
+            {
+                Name = p.Name,
+                Price = p.Price,
+                MainDescribe = p.MainDescribe
+            });
+        }
+        //高到低
+        public async Task<IEnumerable<TransportDTO>> TransportOrderByDescendingPrice()
+        {
+            return _dbContext.Products.Where(w => w.Id == 3).OrderByDescending(o => o.Price).Select(p => new TransportDTO
+            {
+                Name = p.Name,
+                Price = p.Price,
+                MainDescribe = p.MainDescribe
+            });
+        }
+    }
+}
---
+++ TravelProject1.0/TravelProject1.0/Models/DTO/AttractionsDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace TravelProject1._0.Models.DTO
+{
+    public class AttractionsDTO
+    {
+        public int ProductId { get; set; }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public decimal Price { get; set; }
+
+        public string? MainDescribe { get; set; }
+    }
+}
