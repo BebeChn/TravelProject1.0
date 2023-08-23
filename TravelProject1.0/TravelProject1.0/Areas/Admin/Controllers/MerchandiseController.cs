6af6731feec3a78df7996cdb9052fbfc1abdbadc
VMWARE\yu <yu20334.sam@gmail.com>
2023-08-23 20:31:41 +0800

    7個

--- TravelProject1.0/TravelProject1.0/Areas/Admin/Controllers/MerchandiseController.cs
+++ TravelProject1.0/TravelProject1.0/Areas/Admin/Controllers/MerchandiseController.cs
@@ -1,19 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TravelProject1._0.Models;
 
 namespace TravelProject1._0.Areas.Admin.Controllers
 {
+
     [Area("Admin")]
+    [Route("api/[controller]/[action]")]
+    [ApiController]
 
     public class MerchandiseController : Controller
     {
-        public IActionResult Index()
+        private readonly TravelProjectAzureContext _db;
+
+        public MerchandiseController(TravelProjectAzureContext db)
         {
-            return View();
+            _db = db;
         }
+
+
         public IActionResult Merchandise()
         {
             return View();
         }
-
     }
 }
--- TravelProject1.0/TravelProject1.0/Models/DTO/MerchandiseDTO.cs
+++ TravelProject1.0/TravelProject1.0/Models/DTO/MerchandiseDTO.cs
@@ -3,8 +3,17 @@
     public class MerchandiseDTO
     {
         public int ProductId { get; set; }
-        public int CategoryId { get; set; }
-        public string Name { get; set; }
+
+        public int Id { get; set; }
+
+        public string ProductName { get; set; } = null!;
+
         public decimal Price { get; set; }
+
+        public string? MainDescribe { get; set; }
+
+        public string? SubDescribe { get; set; }
+
+        public string? ShortDescribe { get; set; }
     }
 }
