977a937cca0c8aa991800ad3f01aec032ccda0ef
Lu <eric185800000@gmail.com>
2023-08-23 19:19:50 +0800

    add CheckOut controllers 、view, and CSS; Update Book cshtml.

---
+++ TravelProject1.0/TravelProject1.0/Controllers/Api/BookPlanApiController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TravelProject1._0.Models;
+using TravelProject1._0.Models.DTO;
+
+namespace TravelProject1._0.Controllers.Api
+{
+	[Route("api/[controller]/[action]")]
+	[ApiController]
+	public class BookPlanApiController : ControllerBase
+	{
+		private readonly TravelProjectAzureContext _context;
+
+		public BookPlanApiController(TravelProjectAzureContext context) 
+		{
+			_context = context;
+		}
+		public IEnumerable<BookPlanDTO> GetBook() 
+		{
+			return _context.Products.Where(b => b.Id == 21).Select(b => new BookPlanDTO
+			{
+				ProductName = b.ProductName,
+				Price = b.Price,
+				MainDescribe = b.MainDescribe,
+				ProductId = b.ProductId,
+				ShortDescribe = b.ShortDescribe,
+				SubDescribe = b.SubDescribe,
+
+			});
+		}
+	}
+}
