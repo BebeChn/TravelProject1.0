08638f1403f38e23ce5034a6c35c6cc724c7e249
VMWARE\yu <yu20334.sam@gmail.com>
2023-08-25 21:54:52 +0800

    no message

---
+++ TravelProject1.0/TravelProject1.0/Models/DTO/OrderDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace TravelProject1._0.Models.DTO
+{
+    public class OrderDTO
+    {
+        public int OrderId { get; set; }
+
+        public int UserId { get; set; }
+
+        public DateTime? OrderDate { get; set; }
+
+
+
+
+
+
+
+    }
+}
