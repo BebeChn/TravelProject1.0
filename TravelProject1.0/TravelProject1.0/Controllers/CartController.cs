375d191697f1c2e50e9e4a4e020da924c2181c97
Bebe <fastic55123@gmail.com>
2023-08-30 20:32:01 +0800

    添加購物車後端程式碼

--- TravelProject1.0/TravelProject1.0/Controllers/CartController.cs
+++ TravelProject1.0/TravelProject1.0/Controllers/CartController.cs
@@ -2,14 +2,41 @@
 using Microsoft.EntityFrameworkCore;
 using TravelProject1._0.Helper;
 using TravelProject1._0.Models;
+using TravelProject1._0.Models.ViewModel;
 
 namespace TravelProject1._0.Controllers
 {
     public class CartController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+        private readonly TravelProjectAzureContext _context;
+
+        public CartController(ILogger<HomeController> logger, TravelProjectAzureContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
-        } 
+           
+                //向 Session 取得商品列表
+                List<CartViewModel> CartItems = Session.
+                    GetObjectFromJson<List<CartViewModel>>(HttpContext.Session, "cart");
+
+                //計算商品總額
+                if (CartItems != null)
+                {
+                    ViewBag.Total = CartItems.Sum(m => m.UnitPrice);
+                }
+                else
+                {
+                    ViewBag.Total = 0;
+                }
+
+                return View(CartItems);
+           
+        }
+        
     }
 }
+
