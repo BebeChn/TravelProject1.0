5ef7e0a6e283a4e3db497b138b9639fc5333e422
Bebe <fastic55123@gmail.com>
2023-08-30 19:06:06 +0800

    修改前端版面

--- TravelProject1.0/TravelProject1.0/Models/ViewModel/UpdateUserViewModel.cs
+++ TravelProject1.0/TravelProject1.0/Models/ViewModel/UpdateUserViewModel.cs
@@ -5,7 +5,7 @@ namespace TravelProject1._0.Models.ViewModel
     {
         public int UserId { get; set; }
 
-        public string OldPassword { get; set; }
+        public string Password { get; set; }
 
         public string Name { get; set; } 
 
@@ -18,7 +18,6 @@ namespace TravelProject1._0.Models.ViewModel
 
         public string Phone { get; set; }
 
-        public string? Address { get; set; }
         public string PasswordHash { get; set; } 
         public string Salt { get; set; }
 
