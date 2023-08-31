404491179c69a08c5904bd5189d08b07e40ef4ef
Allen <allen955103@gmail.com>
2023-08-31 19:09:25 +0800

    新增購物車API

--- TravelProject1.0/TravelProject1.0/Controllers/Api/CartApiController.cs
+++ TravelProject1.0/TravelProject1.0/Controllers/Api/CartApiController.cs
@@ -6,38 +6,51 @@ using TravelProject1._0.Models.ViewModel;
 
 namespace TravelProject1._0.Controllers.Api
 {
-    [Route("api/[controller]/[action]")]
+    [Route("api/CartApi/[action]")]
     [ApiController]
     public class CartApiController : ControllerBase
     {
         private readonly ILogger<HomeController> _logger;
         private readonly TravelProjectAzureContext _context;
-
-        private readonly ShoppingCartService _cartService;
-        public CartApiController(ILogger<HomeController> logger, TravelProjectAzureContext context, ShoppingCartService cartService)
+        public CartApiController(ILogger<HomeController> logger, TravelProjectAzureContext context)
         {
             _logger = logger;
             _context = context;
-            _cartService = cartService;
-        }  
-        [HttpPost]
-        public IActionResult AddToCart(int productId, string productName, decimal price, int quantity)
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCart()
         {
-            var item = new ShoppingCartItemViewModel
+            return null;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddToCart([FromBody] CartViewModel model)
+        {
+            if (model == null) return BadRequest();
+
+            Cart item = new Cart
             {
-                Id = productId,
-                ProductName = productName,
-                Price = price,
-                Quantity = quantity
+                UserId = model.UserId,
+                ProductId = model.ProductId,
+                CartName = model.CartName,
+                CartPrice = model.CartPrice,
+                CartQuantity = model.CartQuantity,
+                CartDate = model.CartDate
             };
 
-            _cartService.AddToCart(item);
+            _context.Carts.Add(item);
+
+            try
+            {
+                _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
 
             return Ok(new { Message = "商品已加入購物車" });
         }
-
-        
     }
-
-
 }
