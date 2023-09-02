a4b793e795c832aacbe3631331c7152c8802c078
Bebe <fastic55123@gmail.com>
2023-09-02 16:04:47 +0800

    歷史訂單後端程式碼

--- TravelProject1.0/TravelProject1.0/Models/ViewModel/OrderDetailsDTO.cs
+++ TravelProject1.0/TravelProject1.0/Models/ViewModel/OrderDetailsDTO.cs
@@ -2,8 +2,9 @@
 {
     public class OrderDetailsDTO
     {
-        public int Id { get; set; }
+       
 
+       public int UserId { get; set; }
         public int OrderId { get; set; }
 
         public int PlanId { get; set; }
