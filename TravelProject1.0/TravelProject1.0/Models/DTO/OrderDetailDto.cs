63cb5e23bb4edffd62becd752a1af04946c4d379
Bebe <fastic55123@gmail.com>
2023-09-02 20:35:54 +0800

    增加歷史訂單後端程式碼

---
+++ TravelProject1.0/TravelProject1.0/Models/DTO/OrderDetailDto.cs
@@ -0,0 +1,9 @@
+﻿namespace TravelProject1._0.Models.DTO
+{
+    public class OrderDetailDto
+    {
+        public int PlanId { get; set; }
+        public short? Quantity { get; set; }
+        public decimal? UnitPrice { get; set; }
+    }
+}
