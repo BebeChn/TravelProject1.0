04af6920fed095e89bb4d8d89e58ce65974e5ad1
Sunny <love1780434379@gmail.com>
2023-09-03 12:21:43 +0800

    新增圖表

---
+++ TravelProject1.0/TravelProject1.0/Areas/Admin/Models/ChartViewModel/GetUserAgeGroupDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Permissions;
+
+namespace TravelProject1._0.Areas.Admin.Models.ChartViewModel
+{
+	public class GetUserAgeGroupDTO
+	{
+		public string AgeGroup { get; set; }
+
+		public int Count { get; set; }
+
+	}
+}
--- TravelProject1.0/TravelProject1.0/Controllers/Api/BookPlanApiController.cs
+++ TravelProject1.0/TravelProject1.0/Controllers/Api/BookPlanApiController.cs
@@ -26,7 +26,7 @@ namespace TravelProject1._0.Controllers.Api
 				ShortDescribe = b.ShortDescribe,
 				SubDescribe = b.SubDescribe,
 				Img = b.Img,
-				Longgitude = b.Longgitude,
+				//Longgitude = b.Longgitude,
 				Latitude = b.Latitude
 			});
 		}
