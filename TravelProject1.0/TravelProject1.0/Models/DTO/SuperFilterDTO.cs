d99e203546702b2c1cf3a506b48d02bcf3645684
krn0829 <krn0829@gmail.com>
2023-09-03 13:47:25 +0800

    修改管理者管理頁面的關鍵字查詢

---
+++ TravelProject1.0/TravelProject1.0/Models/DTO/SuperFilterDTO.cs
@@ -0,0 +1,23 @@
+﻿namespace TravelProject1._0.Models.DTO
+{
+	public class SuperFilterDTO
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; } = null!;
+
+		public string Account { get; set; } = null!;
+
+		public string Password { get; set; } = null!;
+
+		public string Role { get; set; } = null!;
+
+		public string Describe { get; set; } = null!;
+
+		public DateTime? CreateDate { get; set; }
+
+		public DateTime? LoginDate { get; set; }
+
+		public int? RoleId { get; set; }
+	}
+}
