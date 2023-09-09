f459ed018695e090c96265a40b4455e046eb328d
krn0829 <krn0829@gmail.com>
2023-09-09 15:39:39 +0800

    管理員管理，新增功能完成

--- TravelProject1.0/TravelProject1.0/Models/DTO/SuperDTO.cs
+++ TravelProject1.0/TravelProject1.0/Models/DTO/SuperDTO.cs
@@ -1,22 +1,21 @@
-﻿namespace TravelProject1._0.Models.DTO
+﻿
+namespace TravelProject1._0.Models.DTO
 {
 	public class SuperDTO
 	{
 		public int Id { get; set; }
 
 		public string Name { get; set; } = null!;
-
 		public string Account { get; set; } = null!;
 
 		public string Password { get; set; } = null!;
 
 		public string Role { get; set; } = null!;
-
 		public string Describe { get; set; } = null!;
 
-		public string? CreateDate { get; set; }
+		public DateTime? CreateDate { get; set; }
 
-		public string? LoginDate { get; set; }
+		public DateTime? LoginDate { get; set; }
 
 		public int? RoleId { get; set; }
 	}
