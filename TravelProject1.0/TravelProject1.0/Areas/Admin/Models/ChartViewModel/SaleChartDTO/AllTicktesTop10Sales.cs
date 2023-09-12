335cf0a2a8279ccce33856535df0721f70d45fd7
Sunny <love1780434379@gmail.com>
2023-09-13 03:56:36 +0800

    新增當月銷售圖表四張，當週銷售Top10

---
+++ TravelProject1.0/TravelProject1.0/Areas/Admin/Models/ChartViewModel/SaleChartDTO/AllTicktesTop10Sales.cs
@@ -0,0 +1,13 @@
+﻿namespace TravelProject1._0.Areas.Admin.Models.ChartViewModel.SaleChartDTO
+{
+	public class AllTicktesTop10Sales
+	{
+			public Dictionary<string,int> AirplaneSale { get; set; }
+
+			public Dictionary<string,int> HotelSale { get; set; }
+
+			public Dictionary<string,int> TransportationSale { get; set; }
+
+			public Dictionary<string,int> AttractionsSale { get; set; }
+	}
+}
