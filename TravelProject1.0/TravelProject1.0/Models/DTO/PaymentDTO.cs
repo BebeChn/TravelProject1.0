4498f7b47e6ad526b3f45bccda3823d61d2b9033
Allen <allen955103@gmail.com>
2023-09-12 21:46:24 +0800

    新增藍新金流controller，修改住宿商品頁面

--- TravelProject1.0/TravelProject1.0/Models/DTO/PaymentDTO.cs
+++ TravelProject1.0/TravelProject1.0/Models/DTO/PaymentDTO.cs
@@ -3,13 +3,19 @@
     public class PaymentDTO
     {
         public IEnumerable<PaymentDetailDTO> detailDTOs { get; set; }
+
         public int Points { get; set; }
+
+        public int TotalPrice { get; set; }
     }
     public class PaymentDetailDTO
     {
         public int PlanId { get; set; }
+
         public int Quantity { get; set; }
+
         public decimal UnitPrice { get; set; }
+
         public string PlanName { get; set; }
     }
 }
