ffd4b415d5043163d9e78ce23f76e02711e29276
Bebe <fastic55123@gmail.com>
2023-09-12 11:19:13 +0800

    修改會員資料日期格式

--- TravelProject1.0/TravelProject1.0/Models/DTO/UpdateUserDTO.cs
+++ TravelProject1.0/TravelProject1.0/Models/DTO/UpdateUserDTO.cs
@@ -8,8 +8,8 @@ namespace TravelProject1._0.Models.DTO
         public string? Name { get; set; }
  
         public string? Email { get; set; }
-        [DataType(DataType.Date)]
-        public DateTime? Birthday { get; set; }
+
+        public string Birthday { get; set; }
  
         public string? Gender { get; set; }
 
