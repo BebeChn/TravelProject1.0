b5cd6a83604cd7eb93c45d350eedf0fd041b54b0
Bebe <fastic55123@gmail.com>
2023-09-16 10:44:07 +0800

    更新管理員商品修改功能新增註冊成功後跳出視窗

--- TravelProject1.0/TravelProject1.0/Areas/Admin/Models/ViewModel/PutProductVIewModel.cs
+++ TravelProject1.0/TravelProject1.0/Areas/Admin/Models/ViewModel/PutProductVIewModel.cs
@@ -2,8 +2,6 @@
 {
     public class PutProductVIewModel
     {
-        public int ProductId { get; set; }
-
         public string ProductName { get; set; }
 
         public decimal Price { get; set; }
