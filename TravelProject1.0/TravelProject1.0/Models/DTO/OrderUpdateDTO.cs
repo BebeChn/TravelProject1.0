483e7938dd8129817f41168f59d06f5abac67bda
Allen <allen955103@gmail.com>
2023-09-17 18:16:31 +0800

    修改管理訂單管理

---
+++ TravelProject1.0/TravelProject1.0/Models/DTO/OrderUpdateDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace TravelProject1._0.Models.DTO
+{
+    public class OrderUpdateDTO
+    {
+        public string Status { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+}
