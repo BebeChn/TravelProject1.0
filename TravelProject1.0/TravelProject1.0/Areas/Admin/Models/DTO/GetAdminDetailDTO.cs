4551423ce912ffcf98ea0ee8cf028499c4f04d73
Bebe <fastic55123@gmail.com>
2023-09-18 13:17:41 +0800

    更改管理員管理頁面

---
+++ TravelProject1.0/TravelProject1.0/Areas/Admin/Models/DTO/GetAdminDetailDTO.cs
@@ -0,0 +1,19 @@
+﻿namespace TravelProject1._0.Areas.Admin.Models.DTO
+{
+    public class GetAdminDetailDTO
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Account { get; set; }
+
+
+        public string Describe { get; set; }
+
+        public DateTime? CreateDate { get; set; }
+
+        public DateTime? LoginDate { get; set; }
+
+    }
+}
